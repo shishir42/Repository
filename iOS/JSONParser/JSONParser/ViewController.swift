b9e3561d3e19c2983894ab65387d8ff0ba0ac614
Shishir <shishir.cse.06@gmail.com>
2017-10-30 07:16:58 +0530

    JOSN

--- iOS/JSONParser/JSONParser/ViewController.swift
+++ iOS/JSONParser/JSONParser/ViewController.swift
@@ -12,7 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // optional chaning
+        
+        //let path = Bundle.main.path(forResource: "usersAPI", ofType: "txt")
+        //print(path ?? "Not a real path")
+        
+        guard let path = Bundle.main.path(forResource: "usersAPI", ofType: "txt") else {return}
+        let url = URL(fileURLWithPath: path)
+        //let data = try! Data(contentsOf: url)
+        
+        do {
+            let data = try Data(contentsOf: url)
+            print(data)
+            //JSON Serialization of data
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            print(json)
+            
+        } catch  {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
