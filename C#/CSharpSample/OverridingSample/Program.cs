a42f84490899205755d643b0fdf5cb925c260e21
Shishir <shishir.cse.06@gmail.com>
2018-05-28 22:43:56 +0530

    c#

--- C#/CSharpSample/OverridingSample/Program.cs
+++ C#/CSharpSample/OverridingSample/Program.cs
@@ -6,13 +6,30 @@ namespace OverridingSample
     {
         public static void Main(string[] args)
         {
-            C c = new C();
-            Console.WriteLine(c.x);
-            Console.WriteLine(((B)c).x);
-            Console.WriteLine(((A)((B)c)).x);
-            c.M1();
-            ((B)c).M1();
-            ((A)((B)c)).M1();
+            //C c = new C();
+            //Console.WriteLine(c.x);
+            //Console.WriteLine(((B)c).x);
+            //Console.WriteLine(((A)((B)c)).x);
+            //c.M1();
+            //((B)c).M1();
+            //((A)((B)c)).M1();
+
+            Parent p = new Parent();
+            p.Test();
+
+            Child c = new Child();
+            c.Test();
+
+            Parent pc = new Child();
+            pc.Test();
+
+            //parent, child, parent
+
+            p.Test1();
+            c.Test1();
+            pc.Test1();
+
+            //parent1, child1, child1
         }
     }
 
@@ -43,4 +60,40 @@ namespace OverridingSample
 			Console.WriteLine("C");
 		}
     }
+
+    class Parent
+    {
+        public Parent()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+
+        static Parent()
+        {
+            Console.WriteLine("Static Constructor");
+        }
+
+        public void Test()
+        {
+            Console.WriteLine("parent");
+        }
+
+        public virtual void Test1()
+        {
+            Console.WriteLine("parent1");
+        }
+    }
+
+    class Child : Parent
+	{
+		public new void Test()
+		{
+			Console.WriteLine("Child");
+		}
+
+        public override void Test1()
+		{
+			Console.WriteLine("child1");
+		}
+	}
 }
